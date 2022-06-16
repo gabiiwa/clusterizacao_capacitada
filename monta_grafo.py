68f890c7074cf13f6f7357ffbb0cec9fcc58e4be
Thiago Almeida <thiago9864@hotmail.com>
2022-06-16 01:13:31 -0300

    pastas das instancias e leitura da ranreal_sparse

--- monta_grafo.py
+++ monta_grafo.py
@@ -27,26 +27,27 @@ class Grafo:
         self.arestas = [] # lista de dicionários, tendo o formato {id, vizinho, peso}
 
     def add_no(self, id, peso):
-        self.grafo[id] = []
-        self.nos.append({'id': id, 'peso': peso})
+        self.grafo[str(id)] = []
+        self.nos.append({'id': str(id), 'peso': peso})
 
     def add_aresta(self, no, no_vizinho, peso):
+        # print('Add aresta {}-{} com peso {}'.format(no, no_vizinho, peso))
         for x, y in [(no, no_vizinho), (no_vizinho, no)]:
-            self.grafo[x].append(y)
-            self.arestas.append({'id': x, 'vizinho': y, 'peso': peso})
+            self.grafo[str(x)].append(y)
+            self.arestas.append({'id': str(x), 'vizinho': str(y), 'peso': peso})
             
 #%%
 # {
 #     '1': {'2','3'}
 #     '2': {'1','3'}
 # }
-g = Grafo()
-g.add_no(1, 6)
-g.add_no(2, 4)
-g.add_no(3, 8)
-print('nos', g.grafo, g.nos)
-g.add_aresta(1,2,3.5)
-g.add_aresta(1,3,4.5)
+# g = Grafo()
+# g.add_no(1, 6)
+# g.add_no(2, 4)
+# g.add_no(3, 8)
+# print('nos', g.grafo, g.nos)
+# g.add_aresta(1,2,3.5)
+# g.add_aresta(1,3,4.5)
 
-print('completo', g.grafo)
-print('arestas', g.arestas)
+# print('completo', g.grafo)
+# print('arestas', g.arestas)
---
+++ ranreal_sparse.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+from monta_grafo import Grafo
+
+class RanRealSparse:
+    def __init__(self, caminho):
+        self._caminho = caminho
+        self.grafo_instancia = Grafo()
+        self.num_arestas = 0
+        #faz a leitura do arquivo da instancia
+        with open(caminho) as instancia:
+            i=0
+            for line in instancia.readlines():
+                ls = line.rstrip()
+                # print(ls)
+                if i==0:
+                    # Lendo a primeira linha
+                    linha1 = ls.split(' ')
+                    # print(linha1)
+                    self.numero_elementos = linha1[0]
+                    self.numero_clusters = linha1[1]
+                    # Obtem limites inferior e superior da soma dos pesos dos nós
+                    self.L = linha1[3]
+                    self.U = linha1[4]
+                    # Obtem os pesos dos nós
+                    lista_pesos_nos = linha1[linha1.index('W')+1:]
+                    # Cria os nós no grafo com os pesos
+                    for i in range(len(lista_pesos_nos)):
+                        self.grafo_instancia.add_no(i, lista_pesos_nos[i])
+                else:
+                    # Cria aresta com peso
+                    no = ls.split(' ')[0]
+                    no_vizinho = ls.split(' ')[1]
+                    peso = ls.split(' ')[2]
+                    self.grafo_instancia.add_aresta(no, no_vizinho, peso)
+                    self.num_arestas += 1
+                i+=1
+
+    def getGrafo(self):
+        return self.grafo_instancia
+
+    def get_L(self):
+        return self.L
+
+    def get_U(self):
+        return self.U
+
+    def __str__(self) -> str:
+        return '--Instância--\nnumero_elementos: {}\nnumero_clusters: {}\nnum_arestas: {}\nL: {}\nU: {}'.format(
+            self.numero_elementos, self.numero_clusters, self.num_arestas, self.L, self.U
+        )
+
+
+inst = RanRealSparse('instancias/ranreal/RanReal480_17.txt')
+
+print(inst)
